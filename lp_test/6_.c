aa991ec59facbafb8de0f7474c4938bedc566337
shiva shankar <shivashankarvaraprasad6@gmail.com>
2021-12-07 23:57:22 +0530

    internal

--- lp_test/6_.c
+++ lp_test/6_.c
@@ -1,1 +1,23 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<time.h>
+
+int main(){
+    int fd;
+    struct stat buf; struct tm dt;
+
+    if(stat("ff.txt",&buf)==0){
+       
+       if(buf.st_mode & R_OK)
+              printf("\nread mode");
+       if(buf.st_mode & W_OK)
+             printf("\nwrite mode");
+       if(buf.st_mode & X_OK)
+          printf("execute mode");
+
+        printf("\nfile sze %ld",buf.st_size);
+    }
+    else
+      printf("err");
+}
